e69ce3b8282cb64ff38aad13aab3c43c728ca8f8
SergiyStebelskiy <sergiy.stebelskiy.01@gmail.com>
2020-09-19 14:36:13 +0300

    added get user posts

--- src/controllers/profile.js
+++ src/controllers/profile.js
@@ -1,9 +1,16 @@
+import mongoose from "mongoose";
 import User from "../models/User";
+import Post from "../models/Post";
 
 const profile = async (req, res) => {
 	try {
+		const posts = await Post.find({ "author._id": mongoose.Types.ObjectId(req.user._id) });
 		const user = await User.findOne({ _id: req.user._id }).select("-password");
-		res.send(user);
+		res.send({
+			...user._doc,
+			posts
+		});
+		console.log(posts);
 	} catch (error) {
 		res.status(400).send(error);
 	}
